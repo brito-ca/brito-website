58a585dce460dd5ec739ea4f59c9c273896d8a5a
rfyujisato <110069912+rfyujisato@users.noreply.github.com>
2023-02-25 23:51:31 -0500

    AboutUsBanner created and first try to apply css modules

--- components/Icon/Icon.jsx
+++ components/Icon/Icon.jsx
@@ -1,17 +1,18 @@
 import Image from 'next/image'
+import styles from '@/styles/Icon.module.css'
 
 const Icon = (props) => {
     const { iconPath, iconWidth, iconHeight, value, description} = props;
     return (
-        <div>
-            <Image  
+        <div className={styles.block}>
+            <Image
                 src={iconPath}
                 alt={description}
                 width={iconWidth}
                 height={iconHeight}
-            />
-            <h1>{value}</h1>
-            <p>{description}</p>
+             />
+            <h1 className={styles.value}>{value}</h1>
+            <p className={styles.description}>{description}</p>
         </div>
     )
 };
--- pages/aboutus.jsx
+++ pages/aboutus.jsx
@@ -1,40 +1,42 @@
-import Banner from "@/components/Banner/Banner"
+import AboutUsBanner from "@/components/AboutUsBanner/AboutUsBanner"
 import Icon from "@/components/Icon/Icon"
+import styles from '@/styles/AboutUs.module.css'
 
 const AboutUs = () => {
     return (
         <main>
-            <Banner 
+            <AboutUsBanner className={styles.text}
                 title='About us'
                 description='Brazilian IT Ottawa (BRITO) is a group of Brazilian professionals with a career focused in 
                 Information Technology living in the National Capital Region, from different backgrounds, 
                 areas of expertise and willingness to help each other by sharing knowledge and experiences, 
                 building a social and professional network.'
             />
+            <div className={styles.icons}>
+                <Icon
+                    iconPath='/credibility.png'
+                    iconWidth={62.15}
+                    iconHeight={75.96}
+                    value='+7'
+                    description='Years in operation'
+                />
 
-            <Icon
-                iconPath='/credibility.png'
-                iconWidth={62.15}
-                iconHeight={75.96}
-                value='+7'
-                description='Years in operation'
-            />
+                <Icon
+                    iconPath='/network.png'
+                    iconWidth={69.06}
+                    iconHeight={65.6}
+                    value='+72'
+                    description='Network events'
+                />
 
-            <Icon
-                iconPath='/network.png'
-                iconWidth={69.06}
-                iconHeight={65.6}
-                value='+72'
-                description='Network events'
-            />
-
-            <Icon
-                iconPath='/participants.png'
-                iconWidth={82.87}
-                iconHeight={41.43}
-                value='+250'
-                description='Participants'
-            />
+                <Icon
+                    iconPath='/participants.png'
+                    iconWidth={82.87}
+                    iconHeight={41.43}
+                    value='+250'
+                    description='Participants'
+                />
+            </div>
         </main>
     )
 };
