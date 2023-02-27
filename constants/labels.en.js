779184ec151392a09e26bc625931edf52043f9b8
Deborah Lacerda <lace0115@algonquinlive.com>
2023-02-27 16:47:40 -0500

    labels contactUs

---
+++ constants/labels.en.js
@@ -0,0 +1,34 @@
+const labels = {
+  contactUs: {
+    title: "Contact us",
+  },
+  chat: {
+    chat: "Chat with us",
+    descriptionChat: "Our friendly team is here to help",
+  },
+  email: {
+    malito: "hi@brito.it",
+    email: "hi@brito.it",
+  },
+  location: {
+    Basedin: "Based in",
+    city: "Ottawa - Ontario",
+  },
+  midia: {
+    visit: "Visit our social media",
+    instagramlink: "#",
+
+    labelinstagram: "BRITO_Instagram",
+    linklinkedin: "https://www.linkedin.com/groups/12058916/",
+
+    labelinkedin: "BRITO_Linkedin",
+    linkfacebook: "https://www.facebook.com/groups/566404256865424",
+
+    labelfacebook: "BRITO_Facebook",
+  },
+  Maple: {
+    labelimgmaple: "mapleleave",
+  },
+};
+
+export default labels;
