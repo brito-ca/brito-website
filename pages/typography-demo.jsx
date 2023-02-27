de0636280822b8798255ea6c0cbbd3c92d753e35
DercilioFontes <dercilioafontes@gmail.com>
2023-02-26 23:31:21 -0500

    Added buttons and colors

--- pages/typography-demo.jsx
+++ pages/typography-demo.jsx
@@ -7,7 +7,9 @@ const TypographyDemo = () => {
           The quick brown fox jumps over the lazy dog.
         </div>
       </div>
+
       <br />
+
       <div>
         Headline
         <br />
@@ -26,29 +28,70 @@ const TypographyDemo = () => {
         h5
         <h5>The quick brown fox jumps over the lazy dog.</h5>
       </div>
+
       <br />
+
       <div>
         Body
         <br />
         b1
-        <div className="b1">
-          The quick brown fox jumps over the lazy dog.
-        </div>
+        <div className="b1">The quick brown fox jumps over the lazy dog.</div>
         b2
-        <div className="b2">
-          The quick brown fox jumps over the lazy dog.
-        </div>
+        <div className="b2">The quick brown fox jumps over the lazy dog.</div>
         b3
-        <div className="b3">
-          The quick brown fox jumps over the lazy dog.
-        </div>
+        <div className="b3">The quick brown fox jumps over the lazy dog.</div>
         b4
-        <div className="b4">
-          The quick brown fox jumps over the lazy dog.
-        </div>
+        <div className="b4">The quick brown fox jumps over the lazy dog.</div>
         b5
-        <div className="b5">
-          The quick brown fox jumps over the lazy dog.
+        <div className="b5">The quick brown fox jumps over the lazy dog.</div>
+      </div>
+
+      <br />
+
+      <div>
+        Button
+        <br />
+        <button className="primary">Click here!</button>
+      </div>
+
+      <br />
+
+      <div>
+        Link
+        <br />
+        <a href="#">Click here!</a>
+      </div>
+
+      <br />
+
+      <div>
+        Button
+        <br />
+        primary
+        <div>
+          <button className="primary">Button text</button>
+        </div>
+        primary outline
+        <div>
+          <button className="primary outline">Button text</button>
+        </div>
+        secundary
+        <div>
+          <button className="secundary">Button text</button>
+        </div>
+        secundary outline
+        <div>
+          <button className="secundary outline">Button text</button>
+        </div>
+        disabled
+        <div>
+          <button disabled>Button text</button>
+        </div>
+        outline disabled
+        <div>
+          <button disabled className="outline">
+            Button text
+          </button>
         </div>
       </div>
     </div>
