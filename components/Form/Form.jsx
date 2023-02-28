71bc2d021919a507922a4f8abf30aea79d1c4bbf
douglasvinicio <douglas.vinicio@hotmail.com>
2023-02-27 23:00:01 -0500

    Added Full Name, Expertise and Company fields along with useState for each one - css code organized

--- components/Form/Form.jsx
+++ components/Form/Form.jsx
@@ -1,18 +1,37 @@
+import { useState } from 'react'
 import styles from '../../styles/Form.module.css'
 
 const Form = () => {
+
+    const [fullName, setFullName] = useState('');
+    const [expertise, setExpertise] = useState('');
+    const [company, setCompany] = useState('');    
+
     return (
         <>
-        <hr />
-        <div className={styles.formContainer}>
-            <h3 className={styles.title}>Want to join Brito's network?</h3>
-            <form>
-        <p className={styles.label}>Your name</p>
-        <input type="text" />
-      
-            </form>
-        </div>
-        </>        
+            <hr />
+            <div className={styles.formContainer}>
+                <h3 className={styles.title}>Want to join Brito's network?</h3>
+                <form action="" method="post">
+                    {/* Needs to implement * character to mandatory fields */ }
+                    <label for="full-name">Your full name</label>
+                    <input
+                        placeholder="E.g. Amanda costa"
+                        value={fullName}
+                    />
+                    <label>Your expertise</label>
+                    <input
+                        placeholder='E.g. UX Designer'
+                        value={expertise}
+                    />
+                    <label>Your company</label>
+                    <input
+                        placeholder='E.g. TD bank'
+                        value={expertise}
+                    />
+                </form>
+            </div>
+        </>
     )
 }
 
