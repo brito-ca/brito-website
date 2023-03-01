1bb9337c9ae5483c48870ebeaffea51fa7e6a1f3
elicius cordeiro <elicius@gmail.com>
2023-02-28 23:21:53 -0500

    created board files (Board.module.css, Board.jsx) and added the component to the index page

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import styles from '@/styles/Home.module.css';
 import Header from '@/components/Header/Header';
 import Banner from '@/components/Banner/Banner';
+import Board from '@/components/Board/Board';
 
 export default function Home() {
   return (
@@ -13,10 +14,11 @@ export default function Home() {
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <main className={styles.container}>
-        <Header />
+
+        {/* <Header />
         <Banner title='Our mission' />
-        <Banner title='Our Vision' />
-        <div className={styles.title}>Home</div>
+        <Banner title='Our Vision' /> */}
+        {/* <div className={styles.title}>Home</div> */}
       </main>
     </>
   );
