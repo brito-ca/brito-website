27c576e8a56257c0f0cbd25c95087e808f468f63
Deborah Lacerda <lace0115@algonquinlive.com>
2023-03-01 15:52:07 -0500

    Organizer

--- components/Banner/Banner.jsx
+++ components/Banner/Banner.jsx
@@ -5,15 +5,14 @@ import stylesbanner from '@/styles/banner.module.css'
 const Banner = (props) => {
   const { title, description } = props;
   return (
-    <div
-    
-    className='flex-row-center'
-  >
+    <div className='flex-row-center'>
     <div className={stylesbanner.banner}>
       <h3 className={stylesbanner.titleBanner} >{title}</h3>
       <div className='body1 b1'>
       <p className={stylesbanner.descriptionBanner}  >{description}</p>
-    </div></div></div>
+    </div>
+    </div>
+    </div>  
   );
 };
 export default Banner;
