4f4dbbc04fb1a68b6bea2c7f621458f600c00c1a
elicius cordeiro <elicius@gmail.com>
2023-02-28 22:44:07 -0500

    added image (Ellipse 146)

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -1,12 +1,15 @@
 import styles from '@/styles/Hero.module.css';
 
 const Hero = (props) => {
-    const { title, text } = props;
+    const { title, text, img_source } = props;
     return (
         <div>
             <div className={styles.hero_container}>
                 <h1 className={styles.welcome_text}>{title}</h1>
                 <p className={styles.intro_text}>{text}</p>
+            </div>
+            <div className={styles.img_container}>
+                <img src={img_source} alt="Parlement" />
             </div>
         </div>
     );
