d3c093b7c6c94150165e65e152fd652f2514c5fd
DercilioFontes <dercilioafontes@gmail.com>
2023-02-28 23:18:06 -0500

    Added initial divs and align-items center

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,10 +1,27 @@
 import Image from "next/image";
-import smallLogo from "../../public/images/logo-small.svg"
+import smallLogo from "../../public/images/logo-small.svg";
+import Link from "next/link";
 
 const Header = () => {
   return (
-    <div>
+    <div className="flex-row-space-between">
       <Image alt="logo small" src={smallLogo} />
+      <div>
+        <a href="#about">About</a>
+        <a href="#join-us">Join us</a>
+        <a href="#contact">Contact</a>
+      </div>
+      <div>
+        <Link href="/en/home" className="uppercase">
+          en
+        </Link>
+        <Link href="/pt/home" className="uppercase">
+          pt
+        </Link>
+        <Link href="/fr/home" className="uppercase">
+          fr
+        </Link>
+      </div>
     </div>
   );
 };
