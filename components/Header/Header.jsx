fb8becd0be27a6863bfea08e93c1d8e52b2acd62
DercilioFontes <dercilioafontes@gmail.com>
2023-02-28 23:36:45 -0500

    Added initial nav and language stayle, and added more margin and padding sizes

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,21 +1,29 @@
+import styles from "@/styles/Header.module.css";
 import Image from "next/image";
 import smallLogo from "../../public/images/logo-small.svg";
 import Link from "next/link";
 
 const Header = () => {
   return (
-    <div className="flex-row-space-between">
+    <div className="flex-row-space-between padding">
       <Image alt="logo small" src={smallLogo} />
-      <div>
-        <a href="#about">About</a>
-        <a href="#join-us">Join us</a>
-        <a href="#contact">Contact</a>
+      <div className={styles.empty}></div>
+      <div className={styles.nav}>
+        <a href="#about" className="horizontal-margin">
+          About
+        </a>
+        <a href="#join-us" className="horizontal-margin">
+          Join us
+        </a>
+        <a href="#contact" className="horizontal-margin">
+          Contact
+        </a>
       </div>
       <div>
         <Link href="/en/home" className="uppercase">
           en
         </Link>
-        <Link href="/pt/home" className="uppercase">
+        <Link href="/pt/home" className="uppercase horizontal-margin-sm">
           pt
         </Link>
         <Link href="/fr/home" className="uppercase">
