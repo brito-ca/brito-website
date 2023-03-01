dbca97ce8e32c1b5f7c68c79da4242a8aa047e29
Deborah Lacerda <lace0115@algonquinlive.com>
2023-03-01 15:30:49 -0500

    Add style

--- components/Banner/Banner.jsx
+++ components/Banner/Banner.jsx
@@ -5,10 +5,15 @@ import stylesbanner from '@/styles/banner.module.css'
 const Banner = (props) => {
   const { title, description } = props;
   return (
+    <div
+    
+    className='flex-row-center'
+  >
     <div className={stylesbanner.banner}>
       <h3 className={stylesbanner.titleBanner} >{title}</h3>
-      <p className={stylesbanner.descriptionBanner}>{description}</p>
-    </div>
+      <div className='body1 b1'>
+      <p className={stylesbanner.descriptionBanner}  >{description}</p>
+    </div></div></div>
   );
 };
 export default Banner;
