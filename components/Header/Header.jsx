9fa7359e2666711de6f7da844ac61b6b064c380d
DercilioFontes <dercilioafontes@gmail.com>
2023-03-01 00:29:39 -0500

    Styled nav and language and handled active language

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -2,13 +2,20 @@ import styles from "@/styles/Header.module.css";
 import Image from "next/image";
 import smallLogo from "../../public/images/logo-small.svg";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const Header = () => {
+  const router = useRouter();
+  const lng = router.pathname.split("/")[0];
+  const isEn = lng === "en" || lng === "";
+  const isPr = lng === "pt";
+  const isFr = lng === "fr";
+
   return (
     <div className="flex-row-space-between padding">
       <Image alt="logo small" src={smallLogo} />
       <div className={styles.empty}></div>
-      <div className={styles.nav}>
+      <div className={styles.nav + " body1"}>
         <a href="#about" className="horizontal-margin">
           About
         </a>
@@ -19,14 +26,25 @@ const Header = () => {
           Contact
         </a>
       </div>
-      <div>
-        <Link href="/en/home" className="uppercase">
+      <div className={styles.languages + " body4"}>
+        <Link
+          href="/en/home"
+          className={`uppercase ${isEn ? styles.active : ""}`}
+        >
           en
         </Link>
-        <Link href="/pt/home" className="uppercase horizontal-margin-sm">
+        <Link
+          href="/pt/home"
+          className={`uppercase horizontal-margin-sm ${
+            isPr ? styles.active : ""
+          }`}
+        >
           pt
         </Link>
-        <Link href="/fr/home" className="uppercase">
+        <Link
+          href="/fr/home"
+          className={`uppercase ${isFr ? styles.active : ""}`}
+        >
           fr
         </Link>
       </div>
