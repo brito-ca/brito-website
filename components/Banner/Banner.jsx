b64813407e4b438cde4204065d8d54e28c3c771e
Deborah Lacerda <lace0115@algonquinlive.com>
2023-03-02 14:06:02 -0500

    please remove the empty line and cahen name styles

--- components/Banner/Banner.jsx
+++ components/Banner/Banner.jsx
@@ -1,18 +1,13 @@
-import stylesbanner from '@/styles/Banner.module.css'
-
-
-
+import styles from '@/styles/Banner.module.css'
 const Banner = (props) => {
   const { title, description } = props;
   return (
     <div className='flex-row-center'>
-    <div className={stylesbanner.banner}>
-      <h3 className={stylesbanner.titleBanner} >{title}</h3>
+    <div className={styles.banner}>
+      <h3 className={styles.titleBanner} >{title}</h3>
       <div className='body1 b1'>
-      <p className={stylesbanner.descriptionBanner}  >{description}</p>
-    </div>
-    </div>
-    </div>  
+      <p className={styles.descriptionBanner}  >{description}</p>
+    </div></div></div>  
   );
 };
 export default Banner;
