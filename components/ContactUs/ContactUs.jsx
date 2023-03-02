2dbcbe9b89c32eb86c06a17d9770ef3a7bbf9abe
Deborah Lacerda <lace0115@algonquinlive.com>
2023-03-01 22:24:39 -0500

    add styles

--- components/ContactUs/ContactUs.jsx
+++ components/ContactUs/ContactUs.jsx
@@ -1,26 +1,26 @@
 import labels from '@/constants/labels.en';
-import linkedin from '@/components/ContactUs/img/linkedin.png';
-import instagram from '@/components/ContactUs/img/facebook.png';
-import facebook from '@/components/ContactUs/img/linkedin.png';
-import maple from '@/components/ContactUs/img/Vector2.png';
-
+import linkedin from '@/components/ContactUs/image/linkedin.png';
+import instagram from '@/components/ContactUs/image/facebook.png';
+import facebook from '@/components/ContactUs/image/linkedin.png';
+import maple from '@/components/ContactUs/image/Vector2.png';
+import stylesContactUs from '@/styles/ContactUs.module.css'
 const ContactUs = () => {
       return (
         <div>
           <div>
-            <h3>{labels.contactUs.title}</h3>
+            <h3 className={stylesContactUs.title} >{labels.contactUs.title}</h3>
           </div>
-          <div>
-            <h4>{labels.chat.chat}</h4>
-            <p>{labels.chat.descriptionChat}</p>
+          <div className = 'padding'>
+            <h4 className={stylesContactUs.subtitle}>{labels.chat.chat}</h4>
+            <p className='b2 body2'>{labels.chat.descriptionChat}</p>
             <address><a href={labels.email.malito}>{labels.email.email}</a></address>
           </div>
-          <div>
-            <h4>{labels.location.Basedin}</h4>
-            <p>{labels.location.city}</p>
+          <div className = 'padding'>
+            <h4 className={stylesContactUs.subtitle}>{labels.location.Basedin}</h4>
+            <p className='b2 body2'>{labels.location.city}</p>
           </div>
-          <div>
-            <h4>{labels.midia.visit}</h4>
+          <div className = 'padding'>
+            <h4 className={stylesContactUs.subtitle} >{labels.midia.visit}</h4>
             <a href={labels.midia.instagramlink} className="instagram"><img  src={instagram} alt={labels.midia.labelinstagram} /></a>
             <a href={labels.midia.linklinkedin} className="linkedin"><img  src={linkedin} alt={labels.midia.labelinkedin}/></a>
             <a href={labels.midia.linkfacebook} className="facebook"><img  src={facebook} alt={labels.midia.labelfacebook} /></a>
