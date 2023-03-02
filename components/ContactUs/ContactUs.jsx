67776eb817d1e7700edfb278536b181c66d57221
Deborah Lacerda <lace0115@algonquinlive.com>
2023-03-02 10:36:06 -0500

    add icons

--- components/ContactUs/ContactUs.jsx
+++ components/ContactUs/ContactUs.jsx
@@ -1,9 +1,7 @@
-import labels from '@/constants/labels.en';
-import linkedin from '@/components/ContactUs/image/linkedin.png';
-import instagram from '@/components/ContactUs/image/facebook.png';
-import facebook from '@/components/ContactUs/image/linkedin.png';
-import maple from '@/components/ContactUs/image/Vector2.png';
+import labels from '@/constants/labels.en'
+import Image from 'next/image'
 import stylesContactUs from '@/styles/ContactUs.module.css'
+
 const ContactUs = () => {
       return (
         <div>
@@ -21,12 +19,14 @@ const ContactUs = () => {
           </div>
           <div className = 'padding'>
             <h4 className={stylesContactUs.subtitle} >{labels.midia.visit}</h4>
-            <a href={labels.midia.instagramlink} className="instagram"><img  src={instagram} alt={labels.midia.labelinstagram} /></a>
-            <a href={labels.midia.linklinkedin} className="linkedin"><img  src={linkedin} alt={labels.midia.labelinkedin}/></a>
-            <a href={labels.midia.linkfacebook} className="facebook"><img  src={facebook} alt={labels.midia.labelfacebook} /></a>
-            <img  src={maple} alt={labels.midia.labelimgmaple}/>
+            <a href={labels.midia.instagramlink} className="instagram"><Image  width="53" height="53" src="/instagram.png" alt={labels.midia.labelinstagram} /></a>
+            <a href={labels.midia.linklinkedin} className="linkedin"><Image width="53" height="53" src="/linkedin.png"alt={labels.midia.labelinkedin}/></a>
+            <a href={labels.midia.linkfacebook} className="facebook"><Image width="53" height="53" src="/facebook.png" alt={labels.midia.labelfacebook} /></a>
           </div>
-        </div>
+          {/* <div className = 'maple'>
+            <Image  src="/vector2.png" alt="maple" quality="100" layout="fill"/>
+          </div> */}
+      </div>
       );
   };
   export default ContactUs;
