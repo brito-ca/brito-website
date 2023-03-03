afe18c16b5d5babe2078ce711a21474a108be653
Deborah Lacerda <lace0115@algonquinlive.com>
2023-03-02 21:23:30 -0500

    change stile

--- components/ContactUs/ContactUs.jsx
+++ components/ContactUs/ContactUs.jsx
@@ -8,21 +8,32 @@ const ContactUs = () => {
           <div>
             <h3 className={styles.title} >{labels.contactUs.title}</h3>
           </div>
-          <div className = 'padding'>
-            <h4 className={styles.subtitle}>{labels.chat.chat}</h4>
+
+          <div className='padding'>
+            <h4 className={styles.subtitle }>{labels.chat.chat}</h4>
             <p className='b2 body2'>{labels.chat.descriptionChat}</p>
-             <address className = {styles.address}><Image className = {styles.mailimg} width="20" height="16" src="/vector.png" alt="mail" /><a className={styles.mail}  href={labels.email.malito}>{labels.email.email}</a></address>
           </div>
-          <div className = 'padding'>
-            <h4 className={styles.subtitleBasedIn}>{labels.location.Basedin}</h4>
-            <p className='b2 body2'>{labels.location.city}</p>
+
+            <div >
+             <address >
+              <Image className = {styles.mailimg } width="20" height="16" src="/vector.svg" alt="mail" />
+              <a className={styles.mail + ' body3'+' b3'} href={labels.email.malito}>
+                {labels.email.email}
+              </a>
+            </address>
           </div>
-          <div className = 'padding'>
-            <h4 className={styles.subtitle} >{labels.midia.visit}</h4>
-            <div className = {styles.midia}>
-            <a  href={labels.midia.instagramlink} className="instagram"><Image  width="53" height="53" src="/instagram.png" alt={labels.midia.labelinstagram} /></a>
-            <a  href={labels.midia.linklinkedin} className="linkedin"><Image width="53" height="53" src="/linkedin.png"alt={labels.midia.labelinkedin}/></a>
-            <a  href={labels.midia.linkfacebook} className="facebook"><Image width="53" height="53" src="/facebook.png" alt={labels.midia.labelfacebook} /></a>
+          <div className='padding'></div>
+          <div >
+            <h4 className={styles.subtitle+' padding'}>{labels.location.Basedin}</h4>
+            <p className='b2 body2 padding'>{labels.location.city}</p>
+          </div>
+          <div className='padding'></div>
+          <div className='padding'>
+            <h4 className={styles.subtitle } >{labels.midia.visit}</h4>
+            <div className = {styles.midia }>
+            <a  href={labels.midia.instagramlink} className="instagram"><Image  width="53" height="53" src="/instagram.svg" alt={labels.midia.labelinstagram} /></a>
+            <a  href={labels.midia.linklinkedin} className="linkedin"><Image width="53" height="53" src="/linkedin.svg"alt={labels.midia.labelinkedin}/></a>
+            <a  href={labels.midia.linkfacebook} className="facebook"><Image width="53" height="53" src="/facebook.svg" alt={labels.midia.labelfacebook} /></a>
             </div></div>
             <div  className={styles.maple}> </div>
       </div>
