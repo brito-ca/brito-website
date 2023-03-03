16cf167ac4ee7663296236d11798b189f9a8b4df
Deborah Lacerda <lace0115@algonquinlive.com>
2023-03-02 19:34:39 -0500

    remove space and change name syles

--- components/ContactUs/ContactUs.jsx
+++ components/ContactUs/ContactUs.jsx
@@ -1,31 +1,30 @@
 import labels from '@/constants/labels.en'
 import Image from 'next/image'
-import stylesContactUs from '@/styles/ContactUs.module.css'
+import styles from '@/styles/ContactUs.module.css'
 
 const ContactUs = () => {
       return (
         <div>
           <div>
-            <h3 className={stylesContactUs.title} >{labels.contactUs.title}</h3>
+            <h3 className={styles.title} >{labels.contactUs.title}</h3>
           </div>
           <div className = 'padding'>
-            <h4 className={stylesContactUs.subtitle}>{labels.chat.chat}</h4>
+            <h4 className={styles.subtitle}>{labels.chat.chat}</h4>
             <p className='b2 body2'>{labels.chat.descriptionChat}</p>
-            
-            <address className = {stylesContactUs.address}><Image className = {stylesContactUs.mailimg} width="20" height="16" src="/vector.png" alt="mail" /><a className={stylesContactUs.mail}  href={labels.email.malito}>{labels.email.email}</a></address>
+             <address className = {styles.address}><Image className = {styles.mailimg} width="20" height="16" src="/vector.png" alt="mail" /><a className={styles.mail}  href={labels.email.malito}>{labels.email.email}</a></address>
           </div>
           <div className = 'padding'>
-            <h4 className={stylesContactUs.subtitleBasedIn}>{labels.location.Basedin}</h4>
+            <h4 className={styles.subtitleBasedIn}>{labels.location.Basedin}</h4>
             <p className='b2 body2'>{labels.location.city}</p>
           </div>
           <div className = 'padding'>
-            <h4 className={stylesContactUs.subtitle} >{labels.midia.visit}</h4>
-            <div className = {stylesContactUs.midia}>
+            <h4 className={styles.subtitle} >{labels.midia.visit}</h4>
+            <div className = {styles.midia}>
             <a  href={labels.midia.instagramlink} className="instagram"><Image  width="53" height="53" src="/instagram.png" alt={labels.midia.labelinstagram} /></a>
             <a  href={labels.midia.linklinkedin} className="linkedin"><Image width="53" height="53" src="/linkedin.png"alt={labels.midia.labelinkedin}/></a>
             <a  href={labels.midia.linkfacebook} className="facebook"><Image width="53" height="53" src="/facebook.png" alt={labels.midia.labelfacebook} /></a>
             </div></div>
-            <div  className={stylesContactUs.maple}> </div>
+            <div  className={styles.maple}> </div>
       </div>
       );
   };
