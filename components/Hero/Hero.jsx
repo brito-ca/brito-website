4ebea79c5256a4e37977453641b8658b8e6b8fb3
eliciusc <elicius@gmail.com>
2023-03-04 14:57:15 -0500

    Trying to pass image params in the proper way

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -1,8 +1,15 @@
 import styles from '@/styles/Hero.module.css'
-import BannerImage from '@/components/Hero/BannerImage'
+import Image from 'next/image'
+
+const image = {
+    alt: 'parlement',
+    src: '/parlement.png',
+    width: '900',
+    height: '900',
+}
 
 const Hero = (props) => {
-    const { title, description } = props
+    const { title, description, alt = image.alt, src = image.src, width=image.width, height=image.height } = props
     return (
         <div className={styles.container}>
             <div className={styles.hero_container}>
@@ -13,7 +20,15 @@ const Hero = (props) => {
                     <p className={styles.intro_text}>{description}</p>
                 </div>
             </div>
-            <BannerImage/>
+            <div className={styles.crop_img_banner}>
+                <Image
+                    className={styles.img_banner}
+                    src={src}
+                    alt={alt}
+                    width={width}
+                    height={height}
+                />
+            </div>
         </div>
     )
 }
