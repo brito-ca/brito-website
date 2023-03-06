aef1766bcb03eff97fbc4e2666a7286ff8aaeb86
Deborah Lacerda <lace0115@algonquinlive.com>
2023-03-06 10:06:20 -0500

    add div for background

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,6 +2,10 @@ import Head from 'next/head'
 import { AppLayout, Placeholder } from '@/components'
 import Banner from '@/components/Banner/Banner'
 import labels from '@/constants/labels.en'
+import styles from '@/styles/background.module.css'
+
+
+
 export default function Home() {
     return (
         <>
@@ -14,17 +18,21 @@ export default function Home() {
             <AppLayout>
                 <Placeholder title='Hero' />
                 <Placeholder id='about' title='About Us' />
+                <div className={styles.logoOnmission}>
                 <Banner
                     title={labels.ourMission.title}
                     description={labels.ourMission.description}
                 />
                 <Banner title={labels.ourVision.title} description={labels.ourVision.description} />
+                </div>
                 <Placeholder title='Our Board' />
+                <div className={styles.logoOnJoinUs}>
                 <Banner
                     id='join-us'
                     title={labels.joinUs.title}
                     description={labels.joinUs.description}
                 />
+                </div>
                 <Placeholder title='Form' />
                 <Placeholder id='contact' title='Contact Us' />
             </AppLayout>
