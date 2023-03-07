f144cbb7b36049792d871dfbb5854f2c8255bc63
eliciusc <elicius@gmail.com>
2023-03-07 16:14:21 -0500

    passing image object to board component

--- components/Board/Board.jsx
+++ components/Board/Board.jsx
@@ -1,16 +1,18 @@
 import styles from '@/styles/Board.module.css'
 import labels from '@/constants/labels.en'
-import { Icon } from '@/components'
 import Card from '@/components/Board/Card'
 
-const Board = () => {
+const Board = (props) => {
+    const {image} = props
     return (
         <div className={styles.container}>
             <div>
                 <h3 className={styles.title}>{'Our Board'}</h3>
             </div>
-            <div className={'${styles.board_box} flex-row-center'}>
-                <Card/>
+            <div className={styles.board_box}>
+                <Card
+                    image={image.marcelo}
+                />
             </div>
         </div>
     )
