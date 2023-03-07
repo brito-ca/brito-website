5b318a9555cdd381251462016c4a15843ad447b0
rfyujisato <110069912+rfyujisato@users.noreply.github.com>
2023-03-07 17:27:56 -0500

    removed empty div and wrapper

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -9,79 +9,76 @@ import linkedinLogo from '@/public/linkedin-footer.svg'
 
 const Footer = () => {
     return (
-        <footer>
-            <div>
-                <Image src={line} alt='divisor' width={1240} height={3} />
-                <div className={`${styles.navigationContainer} flex-row-space-between`}>
+        <div>
+            <Image src={line} alt='divisor' width={1240} height={3} />
+            <div className={`${styles.footerContainer} flex-row-space-between`}>
+                <div className={styles.logoAndSocial}>
                     <div className={styles.logoAndSocial}>
-                        <div className={styles.logoAndSocial}>
-                            <Image src={britoFlag} alt='logo' width={72.33} height={64.36} />
-                            <Image src={britoWordmark} alt='wordmark' width={193} height={72} />
-                            <p className={'vertical-padding'}>
-                                Building the Brazilian IT community in Ottawa
-                            </p>
-                        </div>
-                        <div className={styles.socialContainer}>
-                            <a
-                                href='https://www.facebook.com/groups/566404256865424'
-                                target={'_blank'}
-                                rel='noopener noreferrer'
-                            >
-                                <Image src={fbLogo} alt='facebook' width={9.82} height={18} />
-                            </a>
-                            <a href='' target={'_blank'} rel='noopener noreferrer'>
-                                <Image src={instaLogo} alt='instagram' width={18} height={18} />
-                            </a>
-                            <a
-                                href='https://www.linkedin.com/groups/12058916/'
-                                target={'_blank'}
-                                rel='noopener noreferrer'
-                            >
-                                <Image src={linkedinLogo} alt='linkedin' width={19} height={18} />
-                            </a>
-                        </div>
-                    </div>
-                    <div></div>
-                    <nav>
-                        <h5 className={styles.title}>Company</h5>
-                        <div>
-                            <p>
-                                <a className={`${styles.navigation} body2`}>About</a>
-                            </p>
-                            <p className={'vertical-padding'}>
-                                <a className={`${styles.navigation} body2`}>Contact</a>
-                            </p>
-                        </div>
-                    </nav>
-                    <nav>
-                        <h5 className={styles.title}>Join us</h5>
-                        <p>
-                            <a className={`${styles.navigation} body2`}>Network list</a>
+                        <Image src={britoFlag} alt='logo' width={72.33} height={64.36} />
+                        <Image src={britoWordmark} alt='wordmark' width={193} height={72} />
+                        <p className={'vertical-padding'}>
+                            Building the Brazilian IT community in Ottawa
                         </p>
-                    </nav>
+                    </div>
+                    <div className={styles.socialContainer}>
+                        <a
+                            href='https://www.facebook.com/groups/566404256865424'
+                            target={'_blank'}
+                            rel='noopener noreferrer'
+                        >
+                            <Image src={fbLogo} alt='facebook' width={9.82} height={18} />
+                        </a>
+                        <a href='' target={'_blank'} rel='noopener noreferrer'>
+                            <Image src={instaLogo} alt='instagram' width={18} height={18} />
+                        </a>
+                        <a
+                            href='https://www.linkedin.com/groups/12058916/'
+                            target={'_blank'}
+                            rel='noopener noreferrer'
+                        >
+                            <Image src={linkedinLogo} alt='linkedin' width={19} height={18} />
+                        </a>
+                    </div>
                 </div>
-                <Image src={line} alt='divisor' width={1240} height={3} />
-                <div className={`${styles.footerBottom} vertical-padding`}>
-                    <p className={styles.copyrightText}>Copyright © 2023 Brito</p>
-                    <p></p>
-                    <div className={styles.footerBottom}>
-                        <p className={styles.copyrightText}>All Rights Reserved</p>
-                        <p>&nbsp;|&nbsp;</p>
+                <nav className={styles.navigationContainer}>
+                    <h5 className={styles.title}>Company</h5>
+                    <div>
                         <p>
-                            <a href='' className={styles.termsLink}>
-                                Terms and Conditions
-                            </a>
+                            <a className={`${styles.navigation} body2`}>About</a>
                         </p>
-                        <p>&nbsp;|&nbsp;</p>
-                        <p>
-                            <a href='' className={styles.termsLink}>
-                                Privacy Policy
-                            </a>
+                        <p className={'vertical-padding'}>
+                            <a className={`${styles.navigation} body2`}>Contact</a>
                         </p>
                     </div>
+                </nav>
+                <nav className={styles.navigationContainer}>
+                    <h5 className={styles.title}>Join us</h5>
+                    <p>
+                        <a className={`${styles.navigation} body2`}>Network list</a>
+                    </p>
+                </nav>
+            </div>
+            <Image src={line} alt='divisor' width={1240} height={3} />
+            <div className={`${styles.footerBottom} vertical-padding`}>
+                <p className={styles.copyrightText}>Copyright © 2023 Brito</p>
+                <p></p>
+                <div className={styles.footerBottom}>
+                    <p className={styles.copyrightText}>All Rights Reserved</p>
+                    <p>&nbsp;|&nbsp;</p>
+                    <p>
+                        <a href='' className={styles.termsLink}>
+                            Terms and Conditions
+                        </a>
+                    </p>
+                    <p>&nbsp;|&nbsp;</p>
+                    <p>
+                        <a href='' className={styles.termsLink}>
+                            Privacy Policy
+                        </a>
+                    </p>
                 </div>
             </div>
-        </footer>
+        </div>
     )
 }
 export default Footer
