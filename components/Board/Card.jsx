f96cce40be79e079e40b1885d7bc73b5af99b9b8
eliciusc <elicius@gmail.com>
2023-03-07 19:49:07 -0500

    Finished board cards

--- components/Board/Card.jsx
+++ components/Board/Card.jsx
@@ -3,31 +3,30 @@ import Image from 'next/image'
 import styles from '@/styles/Board.module.css'
 
 const Card = (props) => {
-    const {image} = props
+    const { image, info } = props
     return (
         <div className={styles.card_box}>
             <div className={styles.image_box}>
-                <Image
-                    src={image.src}
-                    alt={image.alt}
-                    width={image.width}
-                    height={image.height}
-                />
+                <Image src={image.src} alt={image.alt} width={image.width} height={image.height} />
             </div>
-            <div className={styles.title_box}>
-                <h5>Marcelo - Dev director</h5>
-            </div>
-            <div>
-                <p className={styles.info_text}>Senior Front-end Engineer with 20 years of experience building websites and applications using tech-edge for big techs, digital agencies, media and educational companies in Canada, the United States and Brazil.</p>
-            </div>
-            <div className={styles.linkedin_box}>
-                <Icon variant='linkedinCard' />
-            </div>
-            <div className={styles.email_box}>
-                <div>
-                    <Icon variant='mailCard' />
+            <div className={styles.center_box}>
+                <div className={styles.title_box}>
+                    <h5 className={styles.title_text}>{info.title}</h5>
                 </div>
-                <p className='no-padding'>contact@company.com</p>
+                <div>
+                    <p className={styles.info_text}>{info.description}</p>
+                </div>
+            </div>
+            <div className={styles.bottom_box}>
+                <div className={styles.linkedin_box}>
+                    <Icon variant='linkedinCard' />
+                </div>
+                <div className={styles.email_box}>
+                    <div>
+                        <Icon variant='mailCard' />
+                    </div>
+                    <p className='no-padding'>{info.email}</p>
+                </div>
             </div>
         </div>
     )
