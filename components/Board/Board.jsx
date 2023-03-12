899cf1aef7085e953a192e16ef4ed656705987fe
eliciusc <elicius@gmail.com>
2023-03-12 10:23:46 -0400

    Added error boundaries

--- components/Board/Board.jsx
+++ components/Board/Board.jsx
@@ -7,7 +7,7 @@ const Board = (props) => {
         <div className={styles.container}>
             <h3 className={styles.title}>{title}</h3>
             <div className={styles.board_box}>
-                {cards.map((card) => (
+                {cards?.map((card) => (
                     <Card key={card} {...card} />
                 ))}
             </div>
