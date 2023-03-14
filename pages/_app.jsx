8be88d4ebeccf661a1ffc2f71e90659fd13232bf
DercilioFontes <dercilioafontes@gmail.com>
2023-03-13 22:40:35 -0400

    Moved roboto to AppLayout

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,12 +1,6 @@
 import '@/styles/globals.css'
 import '@/styles/form.css'
 import Script from 'next/script'
-import { Roboto } from '@next/font/google'
-
-const roboto = Roboto({
-    subsets: ['latin'],
-    weight: ['300', '400', '500', '700'],
-})
 
 export default function App({ Component, pageProps }) {
     const GA_TAG = 'GTM-586HRW6'
@@ -26,9 +20,7 @@ export default function App({ Component, pageProps }) {
 	                });
                 `}
             </Script>
-            <main className={roboto.className}>
-                <Component {...pageProps} />
-            </main>
+            <Component {...pageProps} />
         </>
     )
 }
