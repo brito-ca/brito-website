2d28ef5e64acfd0d1fe391c01319108e0996f407
DercilioFontes <dercilioafontes@gmail.com>
2023-03-29 21:44:58 -0400

    Added image alt

--- components/AboutUs/AboutUs.jsx
+++ components/AboutUs/AboutUs.jsx
@@ -21,7 +21,7 @@ const AboutUs = (props) => {
                 ))}
             </div>
             <div className={styles.aboutUsImage}>
-                <Image {...membersImage} />
+                <Image alt={membersImage.alt} {...membersImage} />
             </div>
         </div>
     )
