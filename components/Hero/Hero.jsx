893683dd5ebb967cb68319106cc4aac20852b1a1
eliciusc <elicius@gmail.com>
2023-04-03 16:47:42 -0400

    eslint changes

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -4,23 +4,27 @@ import { Sanitize } from '@/components'
 
 const Hero = (props) => {
     const { title, description, image } = props
-    
+
     return (
         <div className={styles.container}>
             <div className={styles.hero_container}>
                 <div className={styles.welcome_box}>
-                    <h3 className={styles.welcome_text}><Sanitize input={title}/></h3>
+                    <h3 className={styles.welcome_text}>
+                        <Sanitize input={title} />
+                    </h3>
                 </div>
                 <div className='body1'>
-                    <p className={styles.intro_text}><Sanitize input={description}/></p>
+                    <p className={styles.intro_text}>
+                        <Sanitize input={description} />
+                    </p>
                 </div>
             </div>
             <div className={styles.crop_img_banner}>
-                <Sanitize input={image.src}/>
-                <Sanitize input={image.alt}/>
-                <Sanitize input={image.width}/>
-                <Sanitize input={image.height}/>
-                <Image className={styles.img_banner} {...image}/>
+                <Sanitize input={image.src} />
+                <Sanitize input={image.alt} />
+                <Sanitize input={image.width} />
+                <Sanitize input={image.height} />
+                <Image className={styles.img_banner} {...image} />
             </div>
         </div>
     )
--- components/Sanitize/Sanitize.jsx
+++ components/Sanitize/Sanitize.jsx
@@ -1,10 +1,8 @@
 import sanitizeHtml from 'sanitize-html'
 
 const Sanitize = (props) => {
-    const {input} = props
+    const { input } = props
 
-    return(
-       sanitizeHtml(input)
-    );
-    
-}; export default Sanitize
+    return sanitizeHtml(input)
+}
+export default Sanitize
