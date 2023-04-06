41590d70166bde8154ca6a32bcb74d5d40650451
rrrainville <roger.rainville@gmail.com>
2023-04-06 12:54:02 -0400

    x

--- components/Form/Form.jsx
+++ components/Form/Form.jsx
@@ -72,18 +72,18 @@ const Form = (props) => {
         }
 
         const res = await fetch(MEMBER_API_URL, options)
-        if (res.status === 200 || res.status === 201) {            
+        if (res.status === 200 || res.status === 201) {
             setSubscriptionModalOpen(!subscriptionModalOpen)
             setFormValues({
                 full_name: '',
                 expertise: '',
-                company: '',                
+                company: '',
                 province: '',
                 city: '',
                 immigration_status: '',
                 linkedin_profile: '',
                 email: '',
-            })            
+            })
         } else {
             alert(
                 `Error: ${res.status} ${res.statusText}  \nSomething went wrong. Please try again.`
@@ -93,7 +93,7 @@ const Form = (props) => {
 
     function getField(fields, id) {
         return fields.find((item) => item?.id === id) || {}
-    }    
+    }
 
     const [immigrationStatusPopover, setImmigrationStatusPopover] = useState(false)
     const [subscriptionModalOpen, setSubscriptionModalOpen] = useState(false)
