7c1ab4780702b151eeae2fc6f306b23e36a9d69e
rrrainville <roger.rainville@gmail.com>
2023-04-14 00:29:31 -0400

    Fix
    #87

---
+++ components/Snackbar/Snackbar.jsx
@@ -0,0 +1,18 @@
+import { useEffect } from 'react'
+import styles from './Snackbar.module.css'
+
+const Snackbar = (props) => {
+    const { isOpen, setIsOpen, type, message } = props
+
+    useEffect(() => {
+        setTimeout(() => { setIsOpen(p => { return { ...p, isOpen: false } }) }, 2900)
+    }, [])
+
+    if (isOpen) {
+        return (
+            <div className={[styles.snackbar, type == 'danger' ? styles.danger : styles.alert].join(" ")}>{message}</div>
+        )
+    }
+}
+
+export default Snackbar
