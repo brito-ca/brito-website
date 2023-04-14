2edb1122ffd704ff1f61918edd91d696bcedd7be
Douglas Vinicio <douglas.vinicio@hotmail.com>
2023-04-14 09:23:14 -0400

    run release

--- services/getBoard.js
+++ services/getBoard.js
@@ -4,10 +4,10 @@ import getMedia from './getMedia'
 const getBoard = async (members) => {
     const results = await fetch(BOARD_API_URL)
     const result = await results.json()
-    const data = members 
+    const data = members
         ? members.map((member) => {
-            return result.find((item) => item.id == member)
-        }) 
+              return result.find((item) => item.id == member)
+          })
         : []
     const cards = await Promise.all(
         data.map(async (item) => {
